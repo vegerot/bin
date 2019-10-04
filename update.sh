09c14286d6ec07c9aea00046b5f6c5ac84d2de10
Max Coplan <mchcopl@gmail.com>
2019-10-04 11:16:29 -0400

    Added App Store updates

--- update.sh
+++ update.sh
@@ -1,73 +1,46 @@
 #!/usr/bin/env bash
-
-function updateBrew()
-{
-    echo "brew upgrade"
-    brew outdated||\
-        brew update&&\
-        read -p 'Would you like to upgrade Brew (y/n): ' upgradeBrew
-            if [[ "$upgradeBrew" == "y" ]]; then
-                brew upgrade
-            fi
-}
-
-function updateVim()
-{
-    echo "vim"
-    vim +PluginUpdate +qall
-}
-
-function updateConda()
-{
-    echo "conda update conda"
-    conda update conda
-    echo "conda update anaconda"
-    conda update anaconda
-    echo 
-    echo "conda update conda"
-    conda update conda
-    echo "conda update anaconda"
-    conda update anaconda
-    echo 
-    echo "conda update --all"
-    conda update --all
-    echo "conda update -n astroconda3 --all"
-    conda update -n astroconda3 --all
-}
-
-function updateMacOS()
-{
-    echo "softwareupdate -l"
-    softwareupdate -l
-    read -p 'Would you like to upgrade macOS (y/n): ' upgradeMacOS
-    if [[ "$upgradeMacOS" == "y" ]]; then
-        softwareupdate -i -a
-    fi
-}
-
-
-
-
-start=`gdate +%s.%N`
-
-if [[ $1 == "--time" ]]; then
-    time updateBrew
-    echo
-    time updateVim
-    echo
-    time updateConda
-    echo
-    time updateMacOS
-else
-    updateBrew
-    echo
-    updateVim
-    echo
-    updateConda
-    echo
-    updateMacOS
+echo "brew upgrade"
+brew outdated||\
+    brew update&&\
+    read -p 'Would you like to upgrade Brew (y/n): ' upgradeBrew
+if [[ "$upgradeBrew" == "y" ]]; then
+    brew upgrade
 fi
-end=`gdate +%s.%N`
 
-run=$(echo "$end - $start"|bc -l)
-echo "$run seconds"
+echo 
+echo "vim"
+
+vim +PluginUpdate +qall
+
+echo 
+echo "conda update conda"
+conda update conda
+echo "conda update anaconda"
+conda update anaconda
+echo 
+echo "conda update conda"
+conda update conda
+echo "conda update anaconda"
+conda update anaconda
+echo 
+echo "conda update --all"
+conda update --all
+echo "conda update -n astroconda3 --all"
+conda update -n astroconda3 --all
+
+echo
+echo "softwareupdate -l"
+softwareupdate -l
+read -p 'Would you like to upgrade macOS (y/n): ' upgradeMacOS
+if [[ "$upgradeMacOS" == "y" ]]; then
+    softwareupdate -i -a --verbose
+fi
+
+
+echo "App Store"
+#open "macappstore://showUpdatesPage"
+mas outdated
+read -p 'Would you like to upgrade apps?  (y/n): ' upgradeApps
+if [[ "$upgradeApps" == "y" ]]; then
+    mas upgrade
+fi
