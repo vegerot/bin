613bfb53cd01c9ad3fa5c2ab49260161de695949
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:12:59 -0400

    Remove bad comment
    Comment was preserved example of bad practice, indented strangely

--- cowCommand.sh
+++ cowCommand.sh
@@ -3,7 +3,6 @@ IFS=$(echo -en ":")
 while : ; do
 
     output="$(whatis `ls $PATH| sort -R|head -1`)" && break
-	#[ "$?" -eq "0" ] && break
 	
 done
 echo $output|cowsay -f $(cowsay -l| tail -n+2|tr " " "\n"|sort -R|head -1)|lolcat
