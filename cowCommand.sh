0d7486fc6cc8dfb34aca3288869c756096ffb654
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:26:24 -0400

    Remove variable only used once

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 IFS=":"
-output="$(whatis `ls $PATH | sort -R | head -1`)"
-echo "$output" | cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | lolcat
+whatis `ls $PATH | sort -R | head -1` | cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | lolcat
