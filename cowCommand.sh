412b371a3e30935dbc3f7c3bbc9e79d27cfe88b8
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:30:04 -0400

    Split commands onto multiple lines

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 IFS=":"
-whatis `ls $PATH | sort -R | head -1` | cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | lolcat
+whatis `ls $PATH | sort -R | head -1` | \
+cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | \
+lolcat
