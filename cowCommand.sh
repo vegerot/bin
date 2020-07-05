e3c0d589369c9d72fca6fd537f36852a8275a1cf
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:58:34 -0400

    Space out tail option argument
    Spaces between options and their first argument aren't necessary but it looks better

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 IFS=":"
 whatis $(find $PATH | sort -R | head -n 1) | \
-cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -n 1) | \
+cowsay -f $(cowsay -l | tail -n +2 | tr " " "\n" | sort -R | head -n 1) | \
 lolcat
