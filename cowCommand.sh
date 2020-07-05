e8678537536a4e305249a5ab5a374e683b5c9aed
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:16:30 -0400

    Quote variable
    Variables that are set quoted should be used quoted unless you know why

--- cowCommand.sh
+++ cowCommand.sh
@@ -3,4 +3,4 @@ IFS=":"
 while : ; do
     output="$(whatis `ls $PATH| sort -R|head -1`)" && break
 done
-echo $output|cowsay -f $(cowsay -l| tail -n+2|tr " " "\n"|sort -R|head -1)|lolcat
+echo "$output"|cowsay -f $(cowsay -l| tail -n+2|tr " " "\n"|sort -R|head -1)|lolcat
