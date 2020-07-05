031aefe4a56bfdcbf7f01bda46cc1184d8fa2875
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:25:24 -0400

    Remove while that always breaks

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 IFS=":"
-while : ; do
-    output="$(whatis `ls $PATH | sort -R | head -1`)" && break
-done
+output="$(whatis `ls $PATH | sort -R | head -1`)"
 echo "$output" | cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | lolcat
