ba45fa7697e5e6f64ea7fd32c9f7e6739cfa8220
Evan McCarter <evanmccarter@gmail.com>
2020-07-05 16:52:07 -0400

    Replace $HOME with ~
    Reflect all other home references
    
    Remove potential need to quote variable

--- checkBlobs.sh
+++ checkBlobs.sh
@@ -5,7 +5,7 @@ while true; do
     echo $stat
     if echo $stat|grep yes ; then
         ~/Downloads/tsschecker -B D101AP -e D68E828E91526 -i 12.1.1 --beta -s -o --save-path ~/Desktop/short/
-        ~/Downloads/tsschecker-latest/tsschecker_macos -d iPhone9,3 -e D68E828E91526 -i 12.1.1 --beta -o -s -m $HOME/Downloads/1211b3/BuildManifest.plist --buildid 16C5050a --save-path ~/Desktop/long/
+        ~/Downloads/tsschecker-latest/tsschecker_macos -d iPhone9,3 -e D68E828E91526 -i 12.1.1 --beta -o -s -m ~/Downloads/1211b3/BuildManifest.plist --buildid 16C5050a --save-path ~/Desktop/long/
         rm -r /tmp/tsschecker
         rm /tmp/ota.json
         rm /tmp/firmware.json
