148f7664e4805877d2c468dfcd8d93572212f654
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:21:48 -0400

    Normalize pipe spacing

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 IFS=":"
 while : ; do
-    output="$(whatis `ls $PATH| sort -R|head -1`)" && break
+    output="$(whatis `ls $PATH | sort -R | head -1`)" && break
 done
-echo "$output"|cowsay -f $(cowsay -l| tail -n+2|tr " " "\n"|sort -R|head -1)|lolcat
+echo "$output" | cowsay -f $(cowsay -l | tail -n+2 | tr " " "\n" | sort -R | head -1) | lolcat
