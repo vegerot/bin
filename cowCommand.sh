fa98dde346fcc0ea8351aab9a137de9cebf49b25
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 17:14:07 -0400

    Remove newlines between nothing
    Blank lines didn't separate anything on same indentation level

--- cowCommand.sh
+++ cowCommand.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 IFS=$(echo -en ":")
 while : ; do
-
     output="$(whatis `ls $PATH| sort -R|head -1`)" && break
-	
 done
 echo $output|cowsay -f $(cowsay -l| tail -n+2|tr " " "\n"|sort -R|head -1)|lolcat
