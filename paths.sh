a61799d587d69389632751b998d4850a4350f992
Evan McCarter <EvanMcCarter@gmail.com>
2020-07-05 20:30:21 -0400

    Sort path prepends before appends

--- paths.sh
+++ paths.sh
@@ -1,10 +1,12 @@
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/lsof/bin:$PATH:/usr/local/opt:/opt:$HOME/bin:/usr/local/anaconda3/bin:/usr/local/sbin"
+
+export PATH="$HOME/.jenv/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+
 export PATH="$PATH:$HOME/Library/Python/3.7/bin:/usr/local/lib/python3.7/site-packages"
 export PATH="$PATH:/usr/local/share"
 export PATH="$PATH:$HOME/.gem/ruby/2.6.0/bin"
-export PATH="$HOME/.jenv/bin:$PATH"
 export PATH="$PATH:$HOME/workspace/spark-2.4.4-bin-hadoop2.7/bin"
 
 export PYTHONPATH='$HOME/Library/Python/3.7/bin'
 export NODE_PATH='$NODE_PATH:/usr/local/lib/node_modules'
-export PATH="/usr/local/opt/ruby/bin:$PATH"
